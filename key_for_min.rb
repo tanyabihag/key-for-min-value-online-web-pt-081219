03683caebf31f0b9eb44ec6975428ce968a8b7e3
Tanya Bihag <tbihag@gmail.com>
2019-08-25 20:11:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,8 @@
 name_hash = {blake: 500, ashley: 2, adam: 1}
 
 def key_for_min_value(name_hash)
-
+  lowest_key = nil
+  lowest_value = Float::INFINITY
   name_hash.each do |k, v|
     if v < lowest_value
       lowest_value = v
